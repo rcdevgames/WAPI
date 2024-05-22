00e9a6ebda7463d9ab7bcef1c0e08f9027105212
Rizky Andhika <17998662+rcdevgames@users.noreply.github.com>
2024-05-22 13:29:37 +0700

    Update main.go

--- src/main.go
+++ src/main.go
@@ -18,6 +18,7 @@ import (
 	"github.com/patrickmn/go-cache"
 	"github.com/rs/zerolog"
 	_ "modernc.org/sqlite"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type server struct {
@@ -82,11 +83,11 @@ func main() {
 		panic(fmt.Sprintf("%q: %s\n", err, sqlStmt))
 	}
 
-	if(*waDebug!="") {
+	if *waDebug != "" {
 		dbLog := waLog.Stdout("Database", *waDebug, true)
-		container, err = sqlstore.New("sqlite", "file:./dbdata/main.db?_foreign_keys=on&_busy_timeout=3000", dbLog)
+		container, err = sqlstore.New("sqlite3", "file:./dbdata/data.db?_foreign_keys=on&_busy_timeout=3000", dbLog)
 	} else {
-		container, err = sqlstore.New("sqlite", "file:./dbdata/main.db?_foreign_keys=on&_busy_timeout=3000", nil)
+		container, err = sqlstore.New("sqlite3", "file:./dbdata/data.db?_foreign_keys=on&_busy_timeout=3000", nil)
 	}
 	if err != nil {
 		panic(err)
